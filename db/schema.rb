6b5c93fe08c4783759bc967f1b1b9ee86af1b755
Fabio Gubert <fgubert@fabios-mbp.home>
2019-07-11 11:48:00 +0100

    fixed credentials
    Co-Authored-By: null <cmnocao@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,6 @@ ActiveRecord::Schema.define(version: 2019_04_13_103119) do
     t.string "address_type"
     t.string "postcode"
     t.integer "customer_id"
-    t.float "latitude"
-    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -47,10 +45,8 @@ ActiveRecord::Schema.define(version: 2019_04_13_103119) do
     t.decimal "total"
     t.integer "invoice"
     t.string "postage_method"
-    t.decimal "postage_price"
     t.text "comment"
     t.integer "customer_id"
-    t.integer "address_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
